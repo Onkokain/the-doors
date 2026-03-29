28e396bcf07ade24f1f5a0189eebdd0cfc1f80a4
Rupraj Baral <ruprajbaral@gmail.com>
2026-03-29 18:03:42 +0545

    conversio

--- scripts/conversio.gd
+++ scripts/conversio.gd
@@ -1,10 +1,10 @@
 extends Node
+@onready var heartbeat: AudioStreamPlayer2D = $"../Utilities/heartbeat"
+@onready var bloodred: AnimationPlayer = $"../Utilities/red flash/AnimationPlayer"
 
-@onready var heartbeat: AudioStreamPlayer2D = $heartbeat
 var was_conversio: bool = false
 
 func _process(_delta: float) -> void:
-	
 	if Global.is_conversio:
 		if not was_conversio:
 			conversio()
@@ -16,7 +16,10 @@ func _process(_delta: float) -> void:
 func conversio():
 	if not heartbeat.playing:
 		heartbeat.play()
+		await get_tree().create_timer(5).timeout
+		bloodred.play("pulse")
 	
 func conversio_disable():
 	if heartbeat.playing:
 		heartbeat.stop()
+		bloodred.stop()
